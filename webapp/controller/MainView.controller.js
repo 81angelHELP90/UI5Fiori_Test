821942d35079b4e0f9fab78bae9c087d83da3687
81angelHELP90 <eludumjuego@gmail.com>
2022-03-14 14:40:25 +0000

    elementos en la vista y fun en el controller

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -9,7 +9,21 @@ sap.ui.define([
 
         return Controller.extend("logaligroup.invoices.controller.MainView", {
             onInit: function () {
+               const oJsonModel = new sap.ui.model.json.JSONModel();
+               const oView = this.getView(); //retorna una isntancia de la vista vinculada a este controller
+               
+                oJsonModel.loadData("./model/SelecionScreenMenu.json"); //colocamos ./ porque en el index.html indicamos "resourceroots" como ./
+                oView.setModel(oJsonModel, "selectionScreen"); //Esto hace posible usar el modelo en la vista a traves del identificador "selectionScreen"
+            },
 
+            onFiter: function(){
+
+            },
+
+            onClearFilters: function(){
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/ShipName", "");
+                oModelSelScreen.setProperty("/CountryKey", "");  
             }
         });
     });
